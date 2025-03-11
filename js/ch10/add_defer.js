891fb79b4ab0ece44da13070d2890a0c66e26a91
khyun9807 <khyun9807@naver.com>
2025-03-11 17:48:43 +0900

    3/11 - js, node.js

---
+++ js/ch10/add_defer.js
@@ -0,0 +1,7 @@
+// window.onload = function () {
+//   document.querySelector('h1').style.backgroundColor = 'red';
+//   document.querySelector('h2').style.color = 'orange';
+// };
+
+document.querySelector('h1').style.backgroundColor = 'red';
+document.querySelector('h2').style.color = 'orange';
---
+++ nodejs/ch11/hello.js
@@ -0,0 +1,5 @@
+function hello(name) {
+  console.log(name + ' 감사합니다');
+}
+
+hello('hong gil dong');
