9e798fcea8aeb24322dffa25d8427f3c1b543dd8
khyun9807 <khyun9807@naver.com>
2025-03-25 16:57:44 +0900

    250325

---
+++ vuejs/ch22/p1/test-vue-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>app component</h2>
+    <hr />
+    <ul>
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+      <CheckboxItem />
+    </ul>
+  </div>
+</template>
+
+<script>
+import CheckboxItem from './components/CheckboxItem.vue'
+
+export default {
+  name: 'App',
+  components: { CheckboxItem },
+}
+</script>
