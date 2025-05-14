aace222eaf9c4773b2baa26652657706114b05f6
khyun9807 <khyun9807@naver.com>
2025-05-14 17:55:08 +0900

    250514

---
+++ java/designpattern/builder/src/HTMLBuilder.java
@@ -0,0 +1,55 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class HTMLBuilder extends Builder {
+    private String filename="untitled.html";
+    private StringBuffer sb=new StringBuffer();
+
+
+    @Override
+    public void makeTitle(String title) {
+        filename=title+".html";
+
+        sb.append("<!DOCTYPE html>\n");
+        sb.append("<html>\n");
+        sb.append("<head>\n");
+        sb.append("<title>"+title+"</title>\n");
+        sb.append("</head>\n");
+        sb.append("<body>\n");
+        sb.append("<h1>"+title+"</h1>\n\n");
+    }
+
+    @Override
+    public void makeString(String str) {
+        sb.append("<p>"+str+"</p>\n\n");
+    }
+
+    @Override
+    public void makeItems(String[] items) {
+        sb.append("<ul>\n");
+
+        for(String s:items){
+            sb.append("<li>"+s+"</li>\n");
+        }
+
+        sb.append("</ul>\n\n");
+    }
+
+    @Override
+    public void close() {
+        sb.append("</body>\n");
+        sb.append("</html>\n");
+
+        try(Writer writer=new FileWriter(filename)){
+            writer.write(sb.toString());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public String getHTMLResult() {
+        return filename;
+    }
+}
---
+++ java/designpattern/builder/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void usage(){
+        System.out.println("Usage: java Main text 텍스트 문서 작성");
+        System.out.println("Usage: java Main html HTML 파일 문서 작성");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
